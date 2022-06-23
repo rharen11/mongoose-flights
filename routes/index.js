476ff12e67629d2e2daef33808e9d491b3d60e75
Rachel Kinzel <rharen11@su.edu>
2022-06-23 07:28:43 -0700

    add button to index view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const router = Router()
 
 /* GET home page. */
-router.get('/flights', function(req, res) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' })
 })
 
